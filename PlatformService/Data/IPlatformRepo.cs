639c645b6da08596dc7c386447aff8a7487a911c
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-11 08:06:29 +0400

    create PlatformRepo

--- PlatformService/Data/IPlatformRepo.cs
+++ PlatformService/Data/IPlatformRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace PlatformService.Data;
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
 
 public interface IPlatformRepo
 {
     bool SaveChanges();
+
+    IEnumerable<Platform> GetAllPlatforms();
+    
+    Platform? GetPlatformById(int id);
+    
+    void CreatePlatform(Platform platform);
 }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("InMemoryDatabase");
 });
 
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,28 +23,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
