f3b6f6c2299d6c05aaa20f443c20016b4de5e488
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-02-11 21:33:03 +0400

    create PlatformController

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -1,13 +1,61 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
+[Route("api/platform")]
+[ApiController]
 public class PlatformController : ControllerBase
 {
+    private readonly IPlatformRepo _repository;
+    private readonly IMapper _mapper;
+
     public PlatformController(IPlatformRepo platformRepo, IMapper mapper)
     {
+        _repository = platformRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> GetPlatforms");
+
+        var platformItem = _repository.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<PlatformReadDto> GetPlatformById([FromRoute] int id)
+    {
+        Console.WriteLine("--> GetPlatformById");
+
+        var platformItem = _repository.GetPlatformById(id);
+
+        if (platformItem == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform([FromBody] PlatformCreateDto platformCreateDto)
+    {
+        Console.WriteLine("--> CreatePlatform");
         
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+        
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+        
+        var platfromReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+        
+        return CreatedAtAction(nameof(GetPlatformById), new { id = platformModel.Id }, platfromReadDto);
     }
 }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,11 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using PlatformService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("InMemoryDatabase");
@@ -20,10 +53,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 PrepDb.PrepPopulation(app);
 
